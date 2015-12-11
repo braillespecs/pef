035b2f3295525cf6e8a101560d011eba9a10e2e4
Joel Håkansson <joel.hakansson@mtm.se>
2015-12-11 15:47:49 +0100

    Moves publishing to braillespecs

--- build-resources/publish.sh
+++ build-resources/publish.sh
@@ -9,15 +9,15 @@ if [ "$TRAVIS_REPO_SLUG" == "joeha480/pef-format" ] && [ "$TRAVIS_PULL_REQUEST" 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet https://${GH_TOKEN}@github.com/joeha480/joeha480.github.io  > /dev/null
+  git clone --quiet https://${GH_TOKEN}@github.com/braillespecs/braillespecs.github.io  > /dev/null
 
-  cd joeha480.github.io
+  cd braillespecs.github.io
   git rm -rf ./pef
   cp -Rf $HOME/site ./pef
   git add -f .
-  git commit -m "Lastest successful travis build of pef-format ($TRAVIS_BUILD_NUMBER) auto-pushed to master"
+  git commit -m "Travis build of pef ($TRAVIS_BUILD_NUMBER) auto-pushed"
   git push -fq origin master > /dev/null
 
-  echo -e "Published site to joeha480.github.io/pef.\n"
+  echo -e "Published site to braillespecs.github.io/pef.\n"
   
 fi
